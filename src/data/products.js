f7355906ea73e24b341077d673571e01ffc15bd7
tubagusivan <ivanbudiman.tb@gmail.com>
2022-07-20 14:02:27 +0700

    card-cart-list

---
+++ src/data/products.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+      id: 1,
+      name: 'Nvidia RTX 2060',
+      price: 349.99,
+      currency: '$',
+      image: 'images/rtx2060.jpg',
+    },
+    {
+      id: 2,
+      name: 'Intel Core i9 9900k',
+      price: 989.99,
+      currency: '$',
+      image: 'images/intel_i9.jpg',
+    },
+    {
+      id: 7,
+      name: 'PC Cooler Intel',
+      price: 49.99,
+      currency: '$',
+      image: 'images/cooler.jpg',
+    },
+    {
+      id: 6,
+      name: 'Hard Drive 3TB',
+      price: 83.72,
+      currency: '$',
+      image: 'images/hard_drive.jpg',
+    },
+    {
+      id: 3,
+      name: 'Full Acer PC',
+      price: 1499.99,
+      currency: '$',
+      image: 'images/fullpc.jpg',
+    },
+    {
+      id: 8,
+      name: 'Full Asus PC',
+      price: 1349.99,
+      currency: '$',
+      image: 'images/fullpc2.jpg',
+    },
+    {
+      id: 4,
+      name: 'Gigabyte B450M DS3H',
+      price: 89.99,
+      currency: '$',
+      image: 'images/gigabyte.jpg',
+    },
+    {
+      id: 5,
+      name: 'RAM Memory Modules',
+      price: 45.99,
+      currency: '$',
+      image: 'images/ram.jpg',
+    },
+    {
+      id: 9,
+      name: 'Nintendo Switch OLED',
+      price: 467.10,
+      currency: '$',
+      image: 'images/nintendo.jpg',
+    }
+  ];
+  
+  export default data;
+  
